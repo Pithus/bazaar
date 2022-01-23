e1d37744067ae309c5f552d2d9199e582b1c932a
Ziv Chen <root@xer.so>
2022-01-24 01:25:41 +0800

    Add API Support

--- bazaar/core/api_view.py
+++ bazaar/core/api_view.py
@@ -1,27 +1,27 @@
 from django.core.files.storage import default_storage
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import views
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
-from rest_framework.status import HTTP_409_CONFLICT, HTTP_201_CREATED
+from rest_framework.decorators import api_view
 
 from bazaar.core.tasks import analyze
 from bazaar.core.utils import get_sha256_of_file
 
 
-class APKFileUploadView(views.APIView):
-    parser_classes = [MultiPartParser]
-    authentication_classes = []
-    permission_classes = []
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    return Response({"message": "Hello!"})
 
-    @csrf_exempt
-    def put(self, request):
-        file_obj = request.data['file']
-        sha256 = get_sha256_of_file(file_obj)
-        if default_storage.exists(sha256):
-            analyze(sha256)
-            return Response(status=HTTP_409_CONFLICT)
-        else:
-            default_storage.save(sha256, file_obj)
-            analyze(sha256)
-            return Response(status=HTTP_201_CREATED)
+@api_view(['POST'])
+def apkupload(request):
+    file_obj = request.data['file']
+    sha256 = get_sha256_of_file(file_obj)
+    if default_storage.exists(sha256):
+        #print(sha256)
+        analyze(sha256)
+        return Response({"file_sha256": sha256})
+    else:
+        default_storage.save(sha256, file_obj)
+        #print(sha256)
+        analyze(sha256)
+        return Response({"file_sha256": sha256})
--- bazaar/core/urls.py
+++ bazaar/core/urls.py
@@ -1,4 +1,9 @@
+from bazaar.core.api_view import hello_world,apkupload
+from django.urls import path
+
 app_name = "core"
 urlpatterns = [
-    # path("upload", view=APKFileUploadView.as_view(), name="upload-apk"),
+    #path("upload", view=APKFileUploadView.as_view(), name="upload-apk"),
+    path("hello",hello_world,name="upload-apk"),
+    path("upload",apkupload,name="upload-apk")
 ]
