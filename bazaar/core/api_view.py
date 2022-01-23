bf7ad3381773c3601397fbd61179794fb4cd6657
Ziv Chen <root@xer.so>
2022-01-24 02:28:05 +0800

    API supports

--- bazaar/core/api_view.py
+++ bazaar/core/api_view.py
@@ -1,8 +1,9 @@
 from django.core.files.storage import default_storage
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from elasticsearch import Elasticsearch
+from django.conf import settings
+import logging
 
 from bazaar.core.tasks import analyze
 from bazaar.core.utils import get_sha256_of_file
@@ -25,3 +26,31 @@ def apkupload(request):
         #print(sha256)
         analyze(sha256)
         return Response({"file_sha256": sha256})
+
+@api_view(['GET', 'POST'])
+def apk_analysis_report(request, sha256):
+    if not request.user.is_authenticated:
+        return Response({"user": "is_authenticated"})
+
+    if request.method == 'GET':
+        es = Elasticsearch(settings.ELASTICSEARCH_HOSTS)
+        try:
+            result = es.get(index=settings.ELASTICSEARCH_APK_INDEX, id=sha256)['_source']
+            return Response(result)
+        except Exception as e:
+            logging.exception(e)
+            return Response({"error":e})
+
+@api_view(['GET', 'POST'])
+def analysis_tasks_status(request, sha256):
+    if not request.user.is_authenticated:
+        return Response({"user": "is_authenticated"})
+
+    if request.method == 'GET':
+        es = Elasticsearch(settings.ELASTICSEARCH_HOSTS)
+        try:
+            result = es.get(index=settings.ELASTICSEARCH_TASKS_INDEX, id=sha256)['_source']
+            return Response(result)
+        except Exception as e:
+            logging.exception(e)
+            return Response({"error":"Object of type NotFoundError is not JSON serializable"})
--- bazaar/core/urls.py
+++ bazaar/core/urls.py
@@ -1,9 +1,11 @@
-from bazaar.core.api_view import hello_world,apkupload
+from bazaar.core.api_view import *
 from django.urls import path
 
 app_name = "core"
 urlpatterns = [
     #path("upload", view=APKFileUploadView.as_view(), name="upload-apk"),
-    path("hello",hello_world,name="upload-apk"),
-    path("upload",apkupload,name="upload-apk")
+    path("hello",hello_world,name="bazaar-api"),
+    path("upload",apkupload,name="bazaar-api"),
+    path("report/<str:sha256>",apk_analysis_report,name="bazaar-api"),
+    path("status/<str:sha256>",analysis_tasks_status,name="bazaar-api")
 ]
