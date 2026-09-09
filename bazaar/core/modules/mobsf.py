e6cec9ffde918b519abdfb81ff1021d350325874
trpyn <emy.rg@protonmail.com>
2026-09-09 16:27:49 +0200

    fix crash on mobsf analysis

--- bazaar/core/modules/mobsf.py
+++ bazaar/core/modules/mobsf.py
@@ -110,12 +110,14 @@ def analysis(sha256):
 
         es.update(index=settings.ELASTICSEARCH_TASKS_INDEX, id=sha256, body={'doc': {'mobsf_analysis': 2}},
                   retry_on_conflict=5)
+
+        del response, to_store
     except Exception as e:
         logging.warning(e)
         es.update(index=settings.ELASTICSEARCH_TASKS_INDEX, id=sha256, body={'doc': {'mobsf_analysis': -1}},
                   retry_on_conflict=5)
 
-    del mobsf, response, to_store
+    del mobsf
     gc.collect()
 
     return {'status': 'success', 'info': ''}
