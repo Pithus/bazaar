94a70bf27e037c1c0917210bbe058b7153e9449d
trpyn <emy.rg@protonmail.com>
2026-09-17 20:06:29 +0200

    flake8²

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.humanize", # Handy template tags
+    "django.contrib.humanize",  # Handy template tags
     "django.contrib.admin",
     "django.forms",
 ]
